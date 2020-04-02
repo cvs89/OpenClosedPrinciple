b665fef269271eb9c59d86998544d6fe66f23e70
cvs rathore <chandraveersingh.89@live.com>
2020-04-02 12:30:02 +0530

    add functions

--- OpenClosedPrinciple/Program.cs
+++ OpenClosedPrinciple/Program.cs
@@ -1,7 +1,47 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace OpenClosedPrinciple
 {
+    public enum Color
+    {
+        Red, Green, Black
+    }
+
+    public enum Size
+    {
+        S, SM, M, XL, XXL, XXXL
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
+        {
+            if(name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+        public class ProductFilter
+        {
+            public static IEnumerable<Product> FilterBySize (IEnumerable<Product> products, Size size)
+            {
+                foreach (var item in products)
+                {
+                    yield return item;
+                }
+                
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
